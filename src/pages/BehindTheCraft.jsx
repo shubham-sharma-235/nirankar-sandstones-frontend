0e5a4efd01a01fafb1a66b84b6f7b1a78c92940f
shubham <shubham.sharma23105@gmail.com>
2026-07-01 13:05:09 +0530

    Fix import case for BehindTheCraft

--- src/pages/BehindTheCraft.jsx
+++ src/pages/BehindTheCraft.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import BreadcrumbSection from "../components/BehindTheCraft/BtsBreadcrum";
-import Artisans from "../components/home/Artisans";
+import Artisans from "../components/Home/Artisans";
 import WorkProcessSection from "../components/BehindTheCraft/WorkProcessSection";
 import OperationalStrengths from "../components/BehindTheCraft/OperationalStrengths";
 
